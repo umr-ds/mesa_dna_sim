281d981328db4a1306a5ee18ea07e7b57a714050
Michael Schwarz <schwarzk@mathematik.uni-marburg.de>
2019-11-06 09:58:33 +0100

    added a way to create and use self-signed certs for ssl

--- start.sh
+++ start.sh
@@ -7,6 +7,14 @@ if [ -n "$CF_Account_ID" ] &  [ -n "$CF_HOSTNAME" ]; then
   sed -i -e 's/mesa.mosla.de/'"$CF_HOSTNAME"'/g' nginx_ssl.conf
   cp nginx_ssl.conf /etc/nginx/nginx.conf
   bash /root/.acme.sh/acme.sh --issue --dns dns_cf -d "$CF_HOSTNAME" --reloadcmd "service nginx force-reload"
+elif [ -n "$SELF_SIGNED_HOSTNAME" ];  then
+ sed -i -e 's/mesa.mosla.de/'"$SELF_SIGNED_HOSTNAME"'/g' nginx_ssl.conf
+ cp nginx_ssl.conf /etc/nginx/nginx.conf
+ mkdir /root/.acme.sh/"$SELF_SIGNED_HOSTNAME"
+ openssl req -newkey rsa:2048 -nodes -keyout /root/.acme.sh/"$SELF_SIGNED_HOSTNAME"/"$SELF_SIGNED_HOSTNAME".key -x509 -days 3650 -out /root/.acme.sh/"$SELF_SIGNED_HOSTNAME"/"$SELF_SIGNED_HOSTNAME".cer -subj "/C=DE/ST=Hessen/L=Marburg/O=MOSLA/CN=$SELF_SIGNED_HOSTNAME"
+ sed -i -e 's/ssl_stapling on;//g' nginx_ssl.conf
+ sed -i -e 's/ssl_stapling_verify on;//g' nginx_ssl.conf
+
 fi
 # -d mesa.mosla.de \
 #--key-file       /path/to/keyfile/in/nginx/key.pem  \
